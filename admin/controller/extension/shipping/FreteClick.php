f00e1177236faacd6151c800f5cca08f1b03d0f5
guicdp7 <guilhermecdp7@gmail.com>
2018-12-12 22:49:58 -0200

    formulario de configurações

--- admin/controller/extension/shipping/FreteClick.php
+++ admin/controller/extension/shipping/FreteClick.php
@@ -1,6 +1,20 @@
 <?php
 class ControllerExtensionShippingFreteClick extends Controller {
-	private $error = array(); 
+	private $error = array();
+	private $campos = array(
+		'FC_CITY_ORIGIN' => "",
+		'FC_CEP_ORIGIN' => "",
+		'FC_STREET_ORIGIN' => "",
+		'FC_NUMBER_ORIGIN' => "",
+		'FC_COMPLEMENT_ORIGIN' => "",
+		'FC_STATE_ORIGIN' => "",
+		'FC_CONTRY_ORIGIN' => "",
+		'FC_DISTRICT_ORIGIN' => "",
+		'FC_INFO_PROD' => "",
+		'FC_SHOP_CART' => "",
+		'FC_API_KEY' => "",
+		'FC_CARRIERS_NUMBER' => ""
+	);
 	
 	public function index() {   
 		$this->load->language('extension/shipping/FreteClick');
@@ -12,7 +26,15 @@ class ControllerExtensionShippingFreteClick extends Controller {
 		
 		$this->load->model('setting/setting');
 
-				
+		if ($this->request->server['REQUEST_METHOD'] == 'POST'){
+			$this->model_setting_setting->editSetting('FreteClick', array("FreteClick_campos" => $this->request->post));
+
+			$this->session->data['success'] = $this->language->get('text_success');			
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true));
+		}
+
+		$data['action'] = $this->url->link('extension/shipping/FreteClick', 'user_token=' . $this->session->data['user_token'], true);
+		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=shipping', true);
 
 		$data['breadcrumbs'] = array();
    		
@@ -30,12 +52,38 @@ class ControllerExtensionShippingFreteClick extends Controller {
 			'text' => $data['heading_title'],
 			'href' => $this->url->link('extension/shipping/FreteClick', 'user_token=' . $this->session->data['user_token'], true)
 		);
-		   
+
+		/*Configuração FreteClick */
+		
+		$result = $this->model_setting_setting->getSetting('FreteClick');
+		$campos = $result["FreteClick_campos"];
+
+		$this->campos['FC_CITY_ORIGIN'] = isset($this->request->post['FC_CITY_ORIGIN']) ? $this->request->post['FC_CITY_ORIGIN'] : $campos['FC_CITY_ORIGIN'];
+        $this->campos['FC_CEP_ORIGIN'] = isset($this->request->post['FC_CEP_ORIGIN']) ? $this->request->post['FC_CEP_ORIGIN'] : $campos['FC_CEP_ORIGIN'];
+		$this->campos['FC_STREET_ORIGIN'] = isset($this->request->post['FC_STREET_ORIGIN']) ? $this->request->post['FC_STREET_ORIGIN'] : $campos['FC_STREET_ORIGIN'];
+		$this->campos['FC_NUMBER_ORIGIN'] = isset($this->request->post['FC_NUMBER_ORIGIN']) ? $this->request->post['FC_NUMBER_ORIGIN'] : $campos['FC_NUMBER_ORIGIN'];
+		$this->campos['FC_COMPLEMENT_ORIGIN'] = isset($this->request->post['FC_COMPLEMENT_ORIGIN']) ? $this->request->post['FC_COMPLEMENT_ORIGIN'] : $campos['FC_COMPLEMENT_ORIGIN'];
+		$this->campos['FC_STATE_ORIGIN'] = isset($this->request->post['FC_STATE_ORIGIN']) ? $this->request->post['FC_STATE_ORIGIN'] : $campos['FC_STATE_ORIGIN'];
+		$this->campos['FC_CONTRY_ORIGIN'] = isset($this->request->post['FC_CONTRY_ORIGIN']) ? $this->request->post['FC_CONTRY_ORIGIN'] : $campos['FC_CONTRY_ORIGIN'];
+		$this->campos['FC_DISTRICT_ORIGIN'] = isset($this->request->post['FC_DISTRICT_ORIGIN']) ? $this->request->post['FC_DISTRICT_ORIGIN'] : $campos['FC_DISTRICT_ORIGIN'];
+		$this->campos['FC_API_KEY'] = isset($this->request->post['FC_API_KEY']) ? $this->request->post['FC_API_KEY'] : $campos['FC_API_KEY'];
+		$this->campos['FC_CARRIERS_NUMBER'] = isset($this->request->post['FC_CARRIERS_NUMBER']) ? $this->request->post['FC_CARRIERS_NUMBER'] : $campos['FC_CARRIERS_NUMBER'];
+
+		$data['data'] = $this->campos;		   
 
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 
 		$this->response->setOutput($this->load->view('extension/shipping/FreteClick', $data));
+	}
+	public function install() {
+		$this->load->model('setting/setting');
+		$result = $this->model_setting_setting->getSetting('FreteClick');
+		if(!$result) {
+			$campos = json_encode($this->campos);
+		   
+			$this->db->query("INSERT INTO `" . DB_PREFIX . "setting` SET store_id = '0', `code` = 'FreteClick', `key` =  'FreteClick_campos', `value` = '". $campos . "', `serialized` = '1'");
+		}
 	}
 }
--- admin/language/en-gb/extension/shipping/FreteClick.php
+++ admin/language/en-gb/extension/shipping/FreteClick.php
@@ -4,11 +4,32 @@ $_['heading_title']       = 'FreteClick';
 
 // Text
 $_['text_extension']   	  	= 'Extensões';
-$_['text_edit']           	= 'Editar o frete pelo FreteClick';
+$_['text_subject']          = 'Configuração do FreteClick';
+$_['text_success'] = 'Configuração FreteClick atualizada com sucesso!';
 
 // Entry
+$_['entry_cep_origin'] = 'CEP de Origem';
+$_['entry_street_origin'] = 'Rua de Origem';
+$_['entry_number_origin'] = 'Número';
+$_['entry_complement_origin'] = 'Complemento';
+$_['entry_district_origin'] = 'Bairro';
+$_['entry_city_origin'] = 'Cidade de Origem';
+$_['entry_state_origin'] = 'Estado de Origem';
+$_['entry_country_origin'] = 'Pais de Origem';
+$_['entry_api_key'] = "API Key";
+$_['entry_carriers_number'] = "Número de Transportadoras";
 
 // Help
+$_['help_cep_origin'] = 'Informe o CEP de onde a mercadoria será coletada';
+$_['help_street_origin'] = 'Informe a Rua de onde a mercadoria será coletada';
+$_['help_number_origin'] = 'Informe o Número do endereço de onde a mercadoria será coletada';
+$_['help_complement_origin'] = 'Informe o Complemento de onde a mercadoria será coletada';
+$_['help_district_origin'] = 'Informe o Bairro de onde a mercadoria será coletada';
+$_['help_city_origin'] = 'Informe a Cidade de onde a mercadoria será coletada';
+$_['help_state_origin'] = 'Informe o Estado de onde a mercadoria será coletada';
+$_['help_country_origin'] = 'Informe o Pais de onde a mercadoria será coletada';
+$_['help_api_key'] = "Informe sua chave de API, disponível em http://www.freteclick.com.br";
+$_['help_carriers_number'] = "Escolha o Número de Transportadoras a serem exibidas em suas cotações";
 
 // Error
 
